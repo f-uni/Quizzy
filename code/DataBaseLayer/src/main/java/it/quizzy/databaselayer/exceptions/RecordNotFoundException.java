455b0436edbc88e47f1c9316bd054d2f3e9358c7
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-01-05 00:03:43 +0100

    update javadoc

--- code/DataBaseLayer/src/main/java/it/quizzy/databaselayer/exceptions/RecordNotFoundException.java
+++ code/DataBaseLayer/src/main/java/it/quizzy/databaselayer/exceptions/RecordNotFoundException.java
@@ -1,5 +1,8 @@
 package it.quizzy.databaselayer.exceptions;
 
+/**
+ * Eccezione invocata quando fallisce il fetch di un record nel db
+ */
 public class RecordNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
--- code/DataBaseLayer/src/main/java/it/quizzy/databaselayer/models/Domanda.java
+++ code/DataBaseLayer/src/main/java/it/quizzy/databaselayer/models/Domanda.java
@@ -2,7 +2,6 @@ package it.quizzy.databaselayer.models;
 
 import java.util.List;
 
-
 import it.quizzy.generated.tables.records.DomandeRecord;
 
 /**
@@ -10,11 +9,23 @@ import it.quizzy.generated.tables.records.DomandeRecord;
  */
 public abstract class Domanda {
 	public DomandeRecord record;
-	
+
+	/**
+	 * @return la stringa contenente la domanda
+	 */
 	public abstract String getDomanda();
+
+	/**
+	 * @return lista di stringhe contenente le possibili risposte
+	 */
 	public abstract List<String> getRisposteDisponibili();
+
+	/**
+	 * @param risp risposta da controllare
+	 * @return true o false se la risposta è rispettivamente corretta o errata
+	 */
 	public abstract boolean controllaRisposta(String risp);
-	
+
 	@Override
 	public String toString() {
 		return record.toString();

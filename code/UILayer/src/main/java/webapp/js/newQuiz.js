a22a205bc6543c7ce183d7f025cbbd0f6fe0c08c
Ellittico <elisa.arnoldi.ea@gmail.com>
2024-01-09 17:19:48 +0100

    fix input

--- code/UILayer/src/main/java/webapp/js/newQuiz.js
+++ code/UILayer/src/main/java/webapp/js/newQuiz.js
@@ -11,18 +11,18 @@ function closePop(option) {
 	if (option == 1) {
 		document.getElementById("questions").innerHTML += '<div class="question" id="QuestVF">' +
 			'<p style="padding:20px"> Question #' + contatore + '</p><textarea class="TextQuestion" rows="4" cols="200"></textarea>' +
-			'<fieldsdet class="rigthAnswer"><legend>Select the right answer:</legend><div><input type="radio" id="1Ans' + contatore + '" checked />' +
-			'<label for="huey">True</label></div><div><input type="radio" id="2Ans' + contatore + '" name="drone" value="dewey" /><label for="dewey">False</label>' +
+			'<fieldsdet class="rigthAnswer"><legend>Select the right answer:</legend><div><input type="radio" id="1Ans' + contatore + '" name="AnswerVF" value="true" checked />' +
+			'<label for="huey">True</label></div><div><input type="radio" id="2Ans' + contatore + '" name="AnswerVF" value="false" /><label for="dewey">False</label>' +
 			'</div></fieldset><button class="delete-button"> Delete </button></div>';
 	} else {
 		document.getElementById("questions").innerHTML += '<div>' +
 			'<div class="question" id="QuestMultiple"><p style="padding:20px"> Question #' + contatore + '</p>' +
 			'<textarea class="TextQuestion" rows="4" cols="200"></textarea><fieldset class="rigthAnswer">' +
-			'<legend style="margin-bottom: 10px;">Select the right answer:</legend><div><input type="radio" id="1Ans' + contatore + '" checked />' +
+			'<legend style="margin-bottom: 10px;">Select the right answer:</legend><div><input type="radio" id="1Ans' + contatore + '" name="answerM" value="ans1" checked />' +
 			'<p class="p-answer"> Answer #1</p><textarea class="TextQuestion other-display" rows="2" cols="100"></textarea>' +
-			'</div><div><input type="radio" id="dewey" name="drone" value="2Ans' + contatore + '" /><p class="p-answer"> Answer #2</p><textarea class="TextQuestion other-display" rows="2" cols="100">' +
-			'</textarea></div><div><input type="radio" id="3Ans' + contatore + '" checked /><p class="p-answer"> Answer #3</p>' +
-			'<textarea class="TextQuestion other-display" rows="2" cols="100"></textarea></div><div><input type="radio" id="4Ans' + contatore + '" name="drone" value="dewey" />' +
+			'</div><div><input type="radio" name="answerM" value="ans2" id="2Ans' + contatore + '" /><p class="p-answer"> Answer #2</p><textarea class="TextQuestion other-display" rows="2" cols="100">' +
+			'</textarea></div><div><input type="radio" id="3Ans' + contatore + '" name="answerM" value="ans3" checked /><p class="p-answer"> Answer #3</p>' +
+			'<textarea class="TextQuestion other-display" rows="2" cols="100"></textarea></div><div><input type="radio" id="4Ans' + contatore + '" name="answerM" value="ans4" />' +
 			'<p class="p-answer"> Answer #4</p><textarea class="TextQuestion other-display" rows="2" cols="100"></textarea>' +
 			'</div></fieldset><button class="delete-button"> Delete</button></div></div>';
 	}

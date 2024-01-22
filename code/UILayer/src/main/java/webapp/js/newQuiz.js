a427407a2afcade11d10ec9b5e0f3a395deccaa5
Ellittico <elisa.arnoldi.ea@gmail.com>
2024-01-22 19:05:39 +0100

    FIXED NEWQUIZ PAGE

--- code/UILayer/src/main/java/webapp/js/newQuiz.js
+++ code/UILayer/src/main/java/webapp/js/newQuiz.js
@@ -1,64 +1,17 @@
-function confirmData() {
-	var data = {
-		titolo: document.getElementById("titoloQuiz").value,
-		domande: []
-	};
+//global variables
+var active=0;
+var contatore=0;
 
-	var questions = document.getElementById("questions").getElementsByClassName("question");
+var confirmedQuest = [];
+var confirmedAnsw = [];
+var confirmedRigthAns = []; 
 
-	for (let q of questions) {
-		if (q.getAttribute("id") === "QuestVF") {
-			var domanda = {
-				tipo: q.getAttribute("id") === "QuestVF" ? 0 : 1,
-				domanda: q.getElementsByClassName("TextQuestion")[0].value,
-				rispostaCorretta: q.getElementsByClassName("rigthAnswer")[0].getElementsByTagName("input")[0].checked ? "vero" : "falso"
-			};
-		} else {
-			var risposte=q.getElementsByClassName("TextAnswer");
-			var stringheRisposte=[];
-			for(let r of risposte){
-				stringheRisposte.push(r.value.toString());
-			}
-			var i=0;
-			for( let  r of q.getElementsByClassName("rigthAnswer")[0].getElementsByTagName("input")){
-				if(r.checked)
-					break;
-				i++
-			}
-			var domanda = {
-				tipo: q.getAttribute("id") === "QuestVF" ? 0 : 1,
-				domanda: q.getElementsByClassName("TextQuestion")[0].value,
-				risposte: stringheRisposte,
-				rispostaCorretta:stringheRisposte[i]
-			};
-		}
-
-		data.domande.push(domanda);
-	}
-	
-	//TODO: inserire controlli
-	
-	fetch("/home/api/creaQuiz.jsp",{
-		method: "POST",
-		headers: {
-		  "Content-Type": "application/json",
-		},
-		body: JSON.stringify(data), 
-	}).then((resp)=>{
-		console.log(resp.status);
-		if(resp.ok)
-			;//redirect
-		else
-			;//errore
-	})
-}
+//opens popups
 function openPop(){
     document.getElementById("popup").classList.remove("popup-display-none");
 }
-var active=0;
-var contatore=0;
-var questions = [];
-var answers = [];
+
+//close popups and creates elements with all data saved by the user
 function closePop(option){
     contatore++;
     document.getElementById("confirmbutton").style.display="block";
@@ -71,7 +24,7 @@ function closePop(option){
             exit;
         }
         document.getElementById("question").innerHTML+='<div class="question" id="QuestVF'+contatore+'">'+
-        '<p style="padding:20px"> Question #'+contatore+'</p><input class="TextQuestion" id="InputQ'+contatore+'">'+
+        '<p style="padding:20px"> Question #'+contatore+'</p><input class="TextQuestion" id="InputQM'+contatore+'">'+
         '<fieldset class="rigthAnswer"><legend>Select the right answer:</legend><div><input type="radio" id="1Ans'+ contatore +'" name="answerVF" value="true"  />'+
         '<label for="1Ans'+contatore+'">True</label></div><div><input type="radio" id="2Ans'+contatore+'" name="answerVF" value="false" /><label for="false">False</label>'+
         '</div></fieldset><button class="delete-button"  onclick="deleteQuest()"> Delete </button>'+
@@ -87,10 +40,10 @@ function closePop(option){
 
         document.getElementById("question").innerHTML+='<div><div class="question" id="QuestMultiple'+contatore+'"><p style="padding:20px"> Question #'+contatore+'</p>'+
         '<input class="TextQuestion"id="InputQM'+contatore+'"><fieldset class="rigthAnswer">'+
-        '<legend style="margin-bottom: 10px;">Select the right answer:</legend><div><input type="radio" id="1Ans'+ contatore +'" />'+
+        '<legend style="margin-bottom: 10px;">Select the right answer:</legend><div><input type="radio" id="1Ans'+ contatore +'" name="drone"/>'+
         '<p class="p-answer"> Answer #1</p><input class="TextQuestion other-display" id="input1'+contatore+'">'+
-        '</div><div><input type="radio" id="dewey" name="drone" value="2Ans'+ contatore +'" /><p class="p-answer"> Answer #2</p><input class="TextQuestion other-display" id="input2'+ contatore+'">'+
-        '</div><div><input type="radio" id="3Ans'+ contatore +'" /><p class="p-answer"> Answer #3</p>'+
+        '</div><div><input type="radio"  name="drone" id="2Ans'+ contatore +'" /><p class="p-answer"> Answer #2</p><input class="TextQuestion other-display" id="input2'+ contatore+'">'+
+        '</div><div><input type="radio"  name="drone" id="3Ans'+ contatore +'" /><p class="p-answer"> Answer #3</p>'+
         '<input class="TextQuestion other-display" id="input3'+contatore+'"></div><div><input type="radio" id="4Ans'+ contatore +'" name="drone" value="dewey" />'+
         '<p class="p-answer"> Answer #4</p><input class="TextQuestion other-display" id="input4'+contatore+'">'+
         '</div></fieldset><button class="delete-button" onclick="deleteQuest()"> Delete</button>'+
@@ -99,58 +52,50 @@ function closePop(option){
     }
 }
 
-
+//saves data from Input when users confirms 
 function saveInput(opt){
     active--;
-    console.log(contatore);
-    var wrap=[];
-    if(opt=="QuestVF"){
-        wrap.push("true");
-        wrap.push("false");
-        var quest = document.getElementById("InputQ"+contatore).value;
-    }
-    else{
-        wrap.push(document.getElementById("input1"+contatore).value);
-        wrap.push(document.getElementById("input2"+contatore).value);
-        wrap.push(document.getElementById("input3"+contatore).value);
-        wrap.push(document.getElementById("input4"+contatore).value);
-        var quest = document.getElementById("InputQM"+contatore).value;
-    }
-    console.log(quest)
-    questions.push(quest);
-    answers.push(wrap);
-    console.log(wrap);
-    console.log(typeof wrap)
     changeContent(opt);
 }
 
-
+function swapContent(opt){
+    var out = [];
+    if(opt=="QuestM"){
+    out[1]=document.getElementById("input1"+contatore).value;
+    out[2]=document.getElementById("input2"+contatore).value;
+    out[3]=document.getElementById("input3"+contatore).value;
+    out[4]=document.getElementById("input4"+contatore).value;
+    }
+    out[0]=document.getElementById("InputQM"+contatore).value;
+    return out
+}
+//Change the place in which the questions are stored
 function changeContent(opt){
+    var output = swapContent(opt);
     var checked = checkOpt(opt);
     if(opt=="QuestVF"){
         document.getElementById("questionWrapped").innerHTML+='<div class="question wrapped" id="'+opt+'Wrapped'+contatore+'">'+
         '<button class="deleteSingleQuestion" onclick="deleteQuest2(this.id)" id="'+ contatore +'"><img class="bin" src="/images/rubbish.png" ></button>'+
-        '<p style="padding:20px" class="titleQ" id="titleQ'+contatore+'" >Question#'+ contatore +": <br>" + questions[contatore-1].slice(0,-1)+'</p><p class="p-answer">'+
+        '<p style="padding:20px" class="titleQ" id="titleQ'+contatore+'">Question#'+ contatore +": <br>" + output[0].slice(0,-1)+'</p><p class="p-answer">'+
         '</p><p class="p-answer is-padded">Answer:</p><br><p class="p-answer is-padded">&#8226; True</p><br><p class="p-answer is-padded"> &#8226; False</p><br>'+
-        '<p class="p-answer is-padded">Right Answer:'+ checked +'</p></div>'
+        '<p class="p-answer is-padded is-right">Right Answer:'+ checked +'</p></div>'
         document.getElementById("question").removeChild(document.getElementById("question").firstChild);
-        console.log(questions[contatore]);
     }
     else{
-        var ans = answers[contatore-1];
         document.getElementById("questionWrapped").innerHTML+='<div class="question wrapped" id="'+opt+'Wrapped'+contatore+'">'+
         '<button class="deleteSingleQuestion" onclick="deleteQuest2(this.id)" id="'+ contatore +'"><img class="bin" src="/images/rubbish.png" ></button>'+
-        '<p style="padding:20px" class="titleQ">Question#'+contatore +": <br>" + questions[contatore-1].slice(0,-1) +'</p><p class="p-answer">'+
-        '</p><p class="p-answer is-padded">Answer:</p><br><p class="p-answer is-padded">&#8226;'+ ans[0].slice(0,-1)+'<br>'+
-        '</p><p class="p-answer is-padded"> &#8226;'+ ans[1].slice(0,-1)+'</p><br>'+
-        '</p><p class="p-answer is-padded"> &#8226;'+ ans[2].slice(0,-1)+'</p><br>'+
-        '</p><p class="p-answer is-padded"> &#8226;'+ ans[3].slice(0,-1)+'</p><br>'+
-        '<p class="p-answer is-padded">Right Answer:'+ checked +'</p></div>'
+        '<p style="padding:20px" class="titleQ" id="titleQ'+contatore+'">Question#'+contatore +": <br>" + output[0]+'</p><p class="p-answer">'+
+        '</p><p class="p-answer is-padded">Answer:</p><br><p class="p-answer is-padded">&#8226;'+ output[1]+'<br>'+
+        '</p><p class="p-answer is-padded"> &#8226;'+ output[2]+'</p><br>'+
+        '</p><p class="p-answer is-padded"> &#8226;'+ output[3]+'</p><br>'+
+        '</p><p class="p-answer is-padded"> &#8226;'+ output[4]+'</p><br>'+
+        '<p class="p-answer is-padded is-right">Right Answer:'+ checked +'</p></div>'
         document.getElementById("question").removeChild(document.getElementById("question").firstChild);
     }
     
 }
 
+//Checks which answers is checked ad correct, in case is not selected is choosen the last one
 function checkOpt(opt){
     var radioCheck;
     document.getElementById(opt+contatore);
@@ -165,50 +110,68 @@ function checkOpt(opt){
     }
     else{
         if(document.getElementById("1Ans"+contatore).checked){
-            radioCheck = document.getElementById("1Ans"+contatore).value;
+            radioCheck = document.getElementById("input1"+contatore).value;
         }
-        if(document.getElementById("1Ans"+contatore).checked){
-            radioCheck = document.getElementById("2Ans"+contatore).value.split(0,-1);
+        else if(document.getElementById("2Ans"+contatore).checked){
+            radioCheck = document.getElementById("input2"+contatore).value;
         }
-        if(document.getElementById("1Ans"+contatore).checked){
-            radioCheck = document.getElementById("3Ans"+contatore).value.split(0,-1);
+        else if(document.getElementById("3Ans"+contatore).checked){
+            radioCheck = document.getElementById("input3"+contatore).value;
+        }
+        else if(document.getElementById("4Ans"+contatore).checked){
+            radioCheck = document.getElementById("input4"+contatore).value;
         }
         else{
-            radioCheck = document.getElementById("4Ans"+contatore).value.split(0,-1);
+            alert("no answer selected");
         }
     }
     return radioCheck;
 }
 
+//Deletes the questions from a button in which the element is not already saved
 function deleteQuest(){
     document.getElementById("question").removeChild(document.getElementById("question").firstChild);
     contatore--;
     active--;
-    answers.pop();
-    questions.pop();
 }
 
+//Deletes the questions from a button in which the element is already saved
 function deleteQuest2(id){
-    //remove a selected div
-    var wrapped = document.getElementsByClassName("wrapped");
-    var ids;
-    var element;
-    for(var i=0;i<wrapped.length;i++){
-        element=wrapped[i]
-        var e = element.id;
-        if(e.includes(id)){
-            ids= element.id;
-        }
+    if(active==1){
+        alert("please confirm before deleting elements");
+        
     }
-    document.getElementById("questionWrapped").removeChild(document.getElementById(ids));
-    //remove arrays questions and answers
-    //Change tile of questions
-    var quest = document.getElementsByClassName("titleQ");
-    var text;
-    for(var j=1; j<=(quest.length) ; j++){
-        if(j!=ids){
-            quest.item(j-1).innerText = "Question#"+j;
-            console.log(quest.item(j-1).innerText)
-        }
+    else{
+        contatore--;
+        //remove a selected div
+       var wrapped = document.getElementsByClassName("wrapped");
+        var ids;
+        var element;
+        for(var i=0;i<wrapped.length;i++){
+            element=wrapped[i]
+           var e = element.id;
+           if(e.includes(id)){
+               ids= element.id;
+          }
+      }
+      document.getElementById("questionWrapped").removeChild(document.getElementById(ids));
+      //remove arrays questions and answers
+      
+      //Change tile of questions
+      var quest = document.getElementsByClassName("titleQ");
+      var text;
+      for(var j=1; j<=(quest.length) ; j++){
+          if(j!=ids){
+              quest.item(j-1).innerText = "Question#"+j;
+          }
+      }
     }
+   
 }
+
+//saves all data in confirmed variables
+function confirmAllData(){
+    confirmedQuest = document.getElementsByClassName("titleQ");
+    confirmedRigthAns = document.getElementsByClassName("is-right");
+}
+    
